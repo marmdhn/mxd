59efe7692408d16b8e4a529e08ede64ecde68c39
Ramadhan <makbarramadhan62@gmail.com>
2024-11-17 02:34:03 +0700

    feat: add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer position-absolute bottom-0">
+      <hr />
+      <p>
+        <i>Archive by</i> <span>marmdhn_</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
