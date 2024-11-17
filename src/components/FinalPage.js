b551bfac3e43cf5832ca618318a98066c467ee4b
Ramadhan <makbarramadhan62@gmail.com>
2024-11-17 10:45:09 +0700

    refactor: add is foreign condition

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-function FinalPage() {
+function FinalPage({ isForeign }) {
   return (
     <motion.div
       className="min-vh-100 d-flex flex-column align-items-center justify-content-center text-white"
@@ -9,13 +9,20 @@ function FinalPage() {
       animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
     >
-      <h1>Terima Kasih</h1>
-      <p>Kamu telah menerima aku sebagai bagian dari hidupmu!</p>
+      <h1> Terima Kasih</h1>
+      <p className="text-center">
+        {isForeign
+          ? "Kamu memilih untuk asing. Semoga kita bertemu kembali di jalan yang lebih baik."
+          : "Kamu telah menerima aku sebagai bagian dari hidupmu!"}
+      </p>
 
       <footer className="footer position-absolute bottom-0">
         <hr />
         <p>
-          <i>Hope</i> <span>we'll be fine together</span>
+          <i>{isForeign ? "OnPlaying" : "Hope"}</i>{" "}
+          <span>
+            {isForeign ? "Juicy Luicy - Asing" : "we'll be fine together"}
+          </span>
         </p>
       </footer>
     </motion.div>
